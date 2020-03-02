7fd609bc0bab51e2aaf9d91d12136c20447a54dd
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-02 13:50:38 -0500

    day 16

---
+++ hackerRank/intToString.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+
+        try 
+        {
+            
+            int test = int.Parse(S);
+            Console.WriteLine(test);
+            
+        } catch (Exception) {
+            Console.WriteLine("Bad String");
+        }
+
+    }
+}
